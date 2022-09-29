db9317991fd7381683811c4a7abd093b0472e1b7
조미정 <ing06164@gmail.com>
2022-09-29 13:09:38 +0900

    CBV로 post목록(post_list) & 상세 페이지(post_detail) 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [   # IP주소/blog/
-    path('', views.index),    # IP주소/blog/
-    path('<int:pk>/', views.single_post_page)  # int형의 pk가 url로 들어온다..
+
+    ### CBV(Class Based View)  이용
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+
+    ### FBV(Function Based View) 이용
+    #path('', views.index),    # IP주소/blog/
+    #path('<int:pk>/', views.single_post_page)  # int형의 pk가 url로 들어온다..
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')     # 모든 post를 가져옴, pk기준으로 최신순으로 정렬(내림차순)
-    return render(request, 'blog/index.html', {'posts': posts})    # 템플릿이 호출될 때 posts 변수도 같이 넘김
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'   # pk로 내림차순으로 정렬(최신순)
+    # 템플릿은 모델명_list.html이 자동으로 불려짐 -> post_list.html
+class PostDetail(DetailView):
+    model = Post
+    # 템플릿 -> post_detail.html이 자동으로 불려짐
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)      # 특정 pk의 post만 가져 옴
-    return render(request, 'blog/single_post_page.html', {'post':post})
+""" FBV """
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')     # 모든 post를 가져옴, pk기준으로 최신순으로 정렬(내림차순)
+#    return render(request, 'blog/index.html', {'posts': posts})    # 템플릿이 호출될 때 posts 변수도 같이 넘김
+
+#def single_post_page(request, pk):
+#    post = Post.objects.get(pk=pk)      # 특정 pk의 post만 가져 옴
+#    return render(request, 'blog/single_post_page.html', {'post':post})
